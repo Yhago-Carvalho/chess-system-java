519b4adf444124de974b1f30fe3afd694ceba3da
Yhago Carvalho <yhago.serafim@quintoandar.com.br>
2022-08-24 21:05:09 -0300

    Printing possible moves

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,10 +14,15 @@ public class Program {
         ChessMatch chessMatch = new ChessMatch();
 
         while (true){
+            UI.clearScreen();
             UI.printBoard(chessMatch.getPieces());
             System.out.println();
             System.out.print("Source: ");
             ChessPosition source = UI.readChessPosition(sc);
+
+            boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+            UI.clearScreen();
+            UI.printBoard(chessMatch.getPieces(), possibleMoves);
 
             System.out.println();
             System.out.print("Target: ");
